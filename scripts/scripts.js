153284b5067bef64492078bc5716061a52f53ffc
Pieter Houtekamer <keaneboots@gmail.com>
2024-09-25 23:53:55 +0200

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,16 +4,5 @@ function updateRecordingCount() {
     document.getElementById('recording-count').textContent = `Total Recordings: ${rowCount}`;
 }
 
-// Function to check if the user is on a mobile device
-function checkMobileDevice() {
-    const isMobile = /Mobi|Android/i.test(navigator.userAgent);
-    if (isMobile) {
-        document.getElementById('mobile-notification').style.display = 'block';
-    }
-}
-
-// Update count and check for mobile device on page load
-document.addEventListener('DOMContentLoaded', () => {
-    updateRecordingCount();
-    checkMobileDevice();
-});
+// Update count on page load
+document.addEventListener('DOMContentLoaded', updateRecordingCount);
