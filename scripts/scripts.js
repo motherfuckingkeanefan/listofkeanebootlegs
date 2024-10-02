e34421cec7fa5314a81653c843003efd11d34d4f
Pieter Houtekamer <keaneboots@gmail.com>
2024-10-02 12:26:22 +0200

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,2 +1,5 @@
-function updateRecordingCount(){const rowCount=document.querySelectorAll('tbody tr').length;document.getElementById('recording-count').textContent=`Total Recordings:${rowCount}`;}
-document.addEventListener('DOMContentLoaded',updateRecordingCount);
+function updateRecordingCount(){
+    const rowCount = document.querySelectorAll('tbody tr').length;
+    document.getElementById('recording-count').textContent = `Total Recordings: ${rowCount}`;
+}
+document.addEventListener('DOMContentLoaded', updateRecordingCount);
