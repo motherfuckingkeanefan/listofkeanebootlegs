b68907681cad30866f698030420ef8fce1ce9eea
Pieter Houtekamer <keaneboots@gmail.com>
2024-10-24 20:28:48 +0200

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -31,7 +31,7 @@ const recordings = [
     { "date": "2004-08-19", "city": "Hasselt-Kiewit", "venue": "Pukkelpop Festival", "source": "FM", "length": 40, "equipment": "Digital Satellite Receiver > CDR > FLAC" },
     { "date": "2004-08-20", "city": "Biddinghuizen", "venue": "Lowlands Festival", "source": "Soundboard", "length": 25, "equipment": "SBD > CDR > FLAC" },
     { "date": "2004-08-21", "city": "Chelmsford", "venue": "V Festival (Backstage Session)", "source": "FM", "length": 10, "equipment": "Digital Satellite Receiver > CDR > FLAC" },
-    { "date": "2004-08-26", "city": "Inverness", "venue": "Up Close Festival", "source": "FM", "length": 45, "equipment": "FM > CDR > FLAC" },
+    { "date": "2004-08-26", "city": "Inverness", "venue": "Up Close Festival", "source": "FM", "length": 40, "equipment": "FM > CDR > FLAC" },
     { "date": "2004-08-29", "city": "Dublin", "venue": "HMV", "source": "Audience", "length": 30, "equipment": "AT831b > Sony MZ-G75" },
     { "date": "2004-08-29", "city": "Dublin", "venue": "Olympia Theatre", "source": "Audience", "length": 35, "equipment": "AT831b > Sony MZ-G755" },
     { "date": "2004-09-08", "city": "Portland", "venue": "Berbati's Pan", "source": "Soundboard", "length": 50, "equipment": "SBD > CDR > FLAC" },
